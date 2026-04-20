0c8d4db7cd1bed7ee261c119e282b7c39026a214
Yanti12699 <Ayencantik185.com>
2026-04-20 08:54:47 +0800

    ini adalah commit pertama

--- index.php
+++ index.php
@@ -1,36 +1,16 @@
 <?php
-echo "Hello Word";
+echo '<br><br><a href="materi1.php"><button> Ke materi 1 : if dan else</button></a>';
+echo '<br><br><a href="materi2.php"><button> Ke materi 2 : looping </button></a>';
+echo '<br><br><a href="materi3.php"><button> Ke materi 1 : looping </button></a>';
+echo '<br><br><a href="materi4.php"><button> Ke materi 1 : looping </button></a>';
+echo '<br><br><a href="materi5.php"><button> Ke materi 1 : looping </button></a>';
+echo '<br><br><a href="materi6.php"><button> Ke materi 1 : looping </button></a>';
+echo '<br><br><a href="materi7.php"><button> Ke materi 1 : looping </button></a>';
+echo '<br><br><a href="materi8.php"><button> Ke materi 1 : looping </button></a>';
+echo '<br><br><a href="materi9.php"><button> Ke materi 1 : looping </button></a>';
+echo '<br><br><a href="materi10.php"><button> Ke materi 1 : looping </button></a>';
 
-echo "<br><br>===============";
-$nilai1 = 11;
-$nilai2 = 12;
-$nilai3 = 5;
-
-$hasil = $nilai1 * $nilai2 - $nilai3;
-
-echo "<br>hasil dari $nilai1 x $nilai2 - $nilai3 adalah $hasil";
-
-if ($hasil >=100){
-    echo "<br>nilai anda lebih dari 100";
-} else if ($hasil < 100) {
-    echo "<br>nilai anda kurang dari 100";
-}else {
-    echo "<br>nilai anda kosong";
-}
-
-
-echo "<br><br>===============";
-echo "<br>hasil dari $nilai1 x $nilai2 - $nilai3 adalah $hasil";
-
-if ($hasil % 2 == 0) {
-    echo "<br>Hasil adalah GENAP";
-} else {
-    echo "<br>Hasil adalah GANJIL";
-}
 ?>
-
-
-
 
 
 
