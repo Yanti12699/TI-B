503fc7b6b96e73a55d1e0cfa517afd2c50dab79a
Yanti12699 <ayencantik185@gmail.com>
2026-04-22 10:30:08 +0800

    Add files via upload

---
+++ materi3.php
@@ -0,0 +1,15 @@
+<?php
+function sapa($nama) {
+    echo "Halo, $nama";
+}
+sapa(" inces");
+?>
+
+function dan return
+<?php
+function tambah($a, $b) {
+    return $a + $b;
+}
+$hasil = tambah(5, 3);
+echo "Hasil: $hasil";
+?>
